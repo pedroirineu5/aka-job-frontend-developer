648faf18431394df14d385fffe769a7f23bc879a
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2024-02-07 19:32:25 -0300

    adicionando um header, para a page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div><p>olá mundo</p></div>
+    <div>
+      <div class="p-10 flex flex-row gap-2 ">
+        <h1 class='basis-1/3 font-extrabold size-8'>AKAPAY</h1>
+        <ul class="basis-1/2 flex flex-row gap-3">
+          <li class="basis-1/3 font-700">Sobre</li>
+          <li class="basis-1/3 font-700">Preços</li>
+          <li class="basis-1/3 font-700">Produto</li>
+        </ul>
+        <button class="basis-1/6 text-white font-medium rounded-full text-md px-2 py-2 text-center me-2 outline outline-offset-1 outline-white  ">Entre</button>
+      </div>
+      <div>
+        
+      </div>
+    </div>
   );
 }
