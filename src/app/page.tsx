023fd60d0f6fcf85b41d46516aca8d25f4db9fba
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2024-02-08 10:41:44 -0300

    troquei a logo de texto para a logo em .png, e comecei a adicionar o meio do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 export default function Home() {
   return (
     <div>
-      <div class="p-10 flex flex-row gap-2 ">
-        <h1 class='basis-1/3 font-extrabold size-8'>AKAPAY</h1>
+      <div class="p-8 flex flex-row gap-1 justify-around ">
+        <img src="/public/images/logoakapay.png" alt="logo akapay" />
         <ul class="basis-1/2 flex flex-row gap-3">
-          <li class="basis-1/3 font-700">Sobre</li>
-          <li class="basis-1/3 font-700">Preços</li>
-          <li class="basis-1/3 font-700">Produto</li>
+          <li class="basis-1/2 text-center align-middle font-700">Sobre</li>
+          <li class="basis-1/2 text-center align-middle font-700">Preços</li>
+          <li class="basis-1/2 text-center align-middle font-700">Produto</li>
         </ul>
-        <button class="basis-1/6 text-white font-medium rounded-full text-md px-2 py-2 text-center me-2 outline outline-offset-1 outline-white  ">Entre</button>
+        <button class="basis-1/6 text-white font-medium rounded-full text-center me-2 outline outline-offset-1 outline-white w-194 h-65 ">Entre</button>
       </div>
-      <div>
-        
+
+    
+      <div class='flex flex-row p-20 md:p-10 sm:p-0.5 '>
+        <div class="w-auto">
+          <p class="font-bold text-base">COMECE DIMINUINDO SUA INADIMPLÊNCIA</p>
+          <h1 class="font-bold text-7xl leading-extra-extra-loose ">Pagamentos de Tributos Nunca  foram tão Fáceis</h1>
+          <span class=" font-bold text-base size-2 ">Lorem ipsum, dolor sit amet consectetur adipisicing   elit. Architecto ad quam et? Incidunt eligendi cum doloremque veniam asperiores illo,   soluta aut, eius vitae voluptatum fugit architecto eveniet eos porro sequi.</span>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
       </div>
+
+
     </div>
   );
 }
